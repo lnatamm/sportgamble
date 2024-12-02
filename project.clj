5fa5efddc8c6659ee8fcf8d494f3c68b0949e9e7
Levi <lnatamm25@gmail.com>
2024-12-02 11:41:48 -0300

    Adicionando dependencias e o main

--- project.clj
+++ project.clj
@@ -2,12 +2,16 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure	"1.10.0"]
-                [compojure	"1.6.1"]
-                [cheshire	"5.8.1"]
-                [ring/ring-defaults	"0.3.2"]
-                [clj-http	"3.9.1"]]
+  :dependencies [
+                  [org.clojure/clojure	"1.10.0"]
+                  [compojure	"1.6.1"]
+                  [cheshire	"5.8.1"]
+                  [ring/ring-defaults	"0.3.2"]
+                  [ring/ring-json	"0.4.0"]
+                  [clj-http	"3.9.1"]
+                ]
   :plugins [[lein-ring "0.12.5"]]
+  :main ^:skip-aot sportgamble.core
   :ring {:handler sportgamble.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api	"2.5"]
