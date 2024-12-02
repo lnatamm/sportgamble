786769d0f0802812ef640e4ec7994e6285477704
Levi <lnatamm25@gmail.com>
2024-12-02 14:28:35 -0300

    Atualizando dependencias

--- src/sportgamble/handler.clj
+++ src/sportgamble/handler.clj
@@ -1,11 +1,29 @@
 (ns sportgamble.handler
-  (:require [compojure.core :refer :all]
-            [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+  (:require [compojure.core	:refer	:all]
+						[compojure.route	:as	route]
+						[cheshire.core	:as	json]
+            [clj-http.client	:as	http]
+            [sportgamble.balancedb :as balancedb]
+            [ring.middleware.json	:refer	[wrap-json-body]]
+            [ring.middleware.defaults	:refer	[wrap-defaults api-defaults]]
+            ))
+
+;Átomo de saldo inicializado com 0
+(def money (atom 0))
+
+(defn as-json [content]
+  {:headers {"Content-Type" "application/json; charset=utf-8"}
+   :body (json/generate-string content)})
+
+(defn deposit[ammount]
+  (swap! money + ammount)
+)
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (POST "/transacoes" ammount (-> (balancedb/register (:body ammount)) (as-json)))
+  (GET "/saldo" [] (as-json {:balance (balancedb/balance)}))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-defaults app-routes api-defaults))
