e40cbe7d7b7dbf7c8e9292878d96116d1a2a1514
Levi <lnatamm25@gmail.com>
2024-12-03 00:40:46 -0300

    Implmentando lógica de registrar transações bem como retorna-las

--- src/sportgamble/balancedb.clj
+++ src/sportgamble/balancedb.clj
@@ -1,16 +1,23 @@
 (ns sportgamble.balancedb)
 
-(def transactions (atom []))
+(defonce transactions (atom []))
 
 (defn calculate[total transaction]
-    (let [ammount (:value transaction)]
-        (+ total ammount)
+    (let [amount (:value transaction)]
+        (+ total amount)
     )
+)
+
+(defn getTransactions[]
+    @transactions
 )
 
 (defn balance []
     (reduce calculate 0 @transactions)
 )
 
-(defn register [transaction]
-  (swap! transaction conj (merge transaction {:id (+ (count @transaction) 1)})))
+(defn register [request]
+    (let [
+            transaction (assoc (:body request) :value (:value request))]
+            (swap! transactions conj transaction)
+            transaction))
